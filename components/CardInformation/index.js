d9620d61890f0e7194b58ad56d07068c1c6dded1
ValentinKt <101396543+ValentinKt@users.noreply.github.com>
2023-11-23 00:06:13 +0100

    Revert "Patch-1"

--- components/CardInformation/index.js
+++
@@ -1,1 +0,0 @@
-export { CardInformation } from "./CardInformation";
--- src/components/ComponontDataList/ComponontDataList.stories.js
+++ src/components/ComponontDataList/ComponontDataList.stories.js
@@ -17,8 +17,12 @@ export const Default = {
     className: {},
     divClassName: {},
     groupClassName: {},
-    frameClassName: {},
-    text: "At vero eos et accusamus et",
+    divClassNameOverride: {},
+    divClassName1: {},
     loremIpsumDolorClassName: {},
+    text:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit<br/>Lorem ipsum dolor sit amet, consectetur adipiscing elit<br/>Lorem ipsum dolor sit amet, consectetur adipiscing elit<br/>Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    frameClassName: {},
+    text1: "At vero eos et accusamus et",
   },
 };
